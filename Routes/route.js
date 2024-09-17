7cc31e2e5579e62a06d3ea2ecf20a7918da689a2
davidnwogucodes <caprisonepapi@gmail.com>
2024-09-17 09:52:12 +0100

    route duplicate fix

--- Routes/route.js
+++ Routes/route.js
@@ -6,15 +6,15 @@ const mealPlans = require("../controllers/mealPlanController");
 const { validate, registerSchema, loginSchema, generateMealPlanSchema, updateUserSchema } = require("../middleware/val");
 const auth = new Auth();
 
-authRouter.post("/api/login/user", validate(loginSchema), userAuth.login);
-authRouter.post("/api/reg/user", validate(registerSchema), userAuth.register);
-authRouter.get("/api/verify-email", userAuth.verifyEmail);
+authRouter.post("/login/user", validate(loginSchema), userAuth.login);
+authRouter.post("/reg/user", validate(registerSchema), userAuth.register);
+authRouter.get("/verify-email", userAuth.verifyEmail);
 authRouter.get("/auth/google", userAuth.googleLogin);
 authRouter.get("/google/callback", userAuth.googleCallback);
 
-authRouter.post("/api/get/generate-meal-plans", validate(generateMealPlanSchema), mealPlans.generateMealPlan);
-authRouter.get("/api/get/past-plans", auth.tokenRequired, mealPlans.getPastMealPlans);
-authRouter.put('/api/update/user', auth.tokenRequired, validate(updateUserSchema), userAuth.editUser);
+authRouter.post("/get/generate-meal-plans", validate(generateMealPlanSchema), mealPlans.generateMealPlan);
+authRouter.get("/get/past-plans", auth.tokenRequired, mealPlans.getPastMealPlans);
+authRouter.put('/update/user', auth.tokenRequired, validate(updateUserSchema), userAuth.editUser);
 
 module.exports = {
   authRouter,
