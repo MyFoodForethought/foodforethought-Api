9e0d4a950718ea06c410901c138e5c3aa0be337b
davidnwogucodes <caprisonepapi@gmail.com>
2024-09-17 16:05:40 +0100

    VE ISH

--- app/index.js
+++ app/index.js
@@ -119,6 +119,9 @@ app.use((err, req, res, next) => {
 
 // Start the server
 const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT_LOCAL || process.env.PORT;
+
+
 
 async function startServer() {
     try {
