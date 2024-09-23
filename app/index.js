f7e84c9d7d87845d136dea77a22753da230b9c38
davidnwogucodes <caprisonepapi@gmail.com>
2024-09-23 16:20:36 +0100

    swagger ehh swagger ahh

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const path = require('path'); 
 const express = require('express');
 const MongoStore = require('connect-mongo');
 const session = require('express-session');
--- swagger.js
+++ swagger.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path'); 
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
