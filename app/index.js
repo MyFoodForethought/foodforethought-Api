7c725ece32faf2e569f4031c4f5d7ad2e6a00318
davidnwogucodes <caprisonepapi@gmail.com>
2024-11-19 11:03:37 +0100

    cors issuex333

--- app/index.js
+++ app/index.js
@@ -87,15 +87,15 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-  // origin: (origin, callback) => {
-  //   if (!origin || allowedOrigins.includes(origin)) {
-  //     callback(null, true);
-  //   } else {
-  //     console.log('Blocked origin:', origin); // Helpful for debugging
-  //     callback(new Error('Not allowed by CORS'));
-  //   }
-  // },
-  origin: true,
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.log('Blocked origin:', origin); // Helpful for debugging
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+ 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
